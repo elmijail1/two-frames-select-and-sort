e1bf5ae3da9cdf9cbb8ab22170428f3111d580b1
elmijail1 <mteplyshev@gmail.com>
2026-08-28 20:04:59 +0400

    fix(frontend): prevent unselected / created items beyond the batch's scope from being appended to the batch

--- frontend/src/handlers/insertItemSorted.ts
+++ frontend/src/handlers/insertItemSorted.ts
@@ -14,25 +14,27 @@ export function insertItemSorted(
 	}
 	let inserted = false;
 	const lastIndex = oldData.pages.length - 1;
-	return {
-		...oldData,
-		pages: oldData.pages.map((page, pageIndex) => {
-			if (inserted) return page;
-			const firstGreater = page.items.find((item) => item.id > id);
-			if (!firstGreater && pageIndex === lastIndex) {
-				return { ...page, items: [...page.items, { id }] };
-			}
-			if (!firstGreater) return page;
-			const index = page.items.indexOf(firstGreater);
-			inserted = true;
-			return {
-				...page,
-				items: [
-					...page.items.slice(0, index),
-					{ id },
-					...page.items.slice(index),
-				],
-			};
-		}),
-	};
+	const everythingLoaded = oldData.pages[lastIndex].newLatestId === null;
+	const pages = oldData.pages.map((page, pageIndex) => {
+		if (inserted) return page;
+		const firstGreater = page.items.find((item) => item.id > id);
+		if (!firstGreater && pageIndex === lastIndex) {
+			if (!everythingLoaded) return page;
+			return { ...page, items: [...page.items, { id }] };
+		}
+		if (!firstGreater) return page;
+
+		const index = page.items.indexOf(firstGreater);
+		inserted = true;
+		return {
+			...page,
+			items: [
+				...page.items.slice(0, index),
+				{ id },
+				...page.items.slice(index),
+			],
+		};
+	});
+
+	return { ...oldData, pages };
 }
