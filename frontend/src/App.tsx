a67a1b60e047a4b3499ec0b95225d4f46272955c
elmijail1 <mteplyshev@gmail.com>
2026-08-30 09:54:05 +0400

    feat(frontend): add basic mobile adaptability

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { FrameSelected } from "./components/FrameSelected";
 import { FrameUnselected } from "./components/FrameUnselected";
+import { useLiveUpdates } from "./hooks/useLiveUpdates";
 
 function App() {
+	useLiveUpdates(true);
 	return (
 		<main>
-			<div className="flex justify-center gap-4 w-[90%] py-8 mx-auto bg-[hsl(150,100%,80%)]">
+			<div className="flex max-md:flex-col max-md:items-center justify-center gap-4 w-[90%] py-8 mx-auto bg-[hsl(150,100%,80%)]">
 				<FrameUnselected />
 				<FrameSelected />
 			</div>
